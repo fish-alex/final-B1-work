09956dbd339836b84acb0e160a6ecc5c22143c05
Alex Fish <alex73fish@yandex.ru>
2024-05-08 08:04:49 +0300

    new file:   .idea/.gitignore 	new file:   .idea/final-B1-work.iml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# MyProfile app for entrepreneurs
+
+SEPARATOR = '------------------------------------------'
+
+# user profile
+name = ''
+age = 0
+phone = ''
+email = ''
+postcode = ''
+address = ''
+info = ''
+# business profile
+ogrnip = 0
+inn = 0
+account = 0
+bank = ''
+bic = 0
+cor_account = 0
+
+print('Приложение MyProfile для предпринимателей')
+print('Сохраняй информацию о себе и выводи ее в разных форматах')
