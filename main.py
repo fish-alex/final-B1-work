7872729c1da212ff571eff2512f39f3a84eda234
Alex Fish <alex73fish@yandex.ru>
2024-05-08 08:15:15 +0300

    updated main.py

--- main.py
+++ main.py
@@ -20,3 +20,15 @@ cor_account = 0
 
 print('Приложение MyProfile для предпринимателей')
 print('Сохраняй информацию о себе и выводи ее в разных форматах')
+
+while True:
+    # main menu
+    print(SEPARATOR)
+    print('ГЛАВНОЕ МЕНЮ')
+    print('1 - Ввести или обновить информацию')
+    print('2 - Вывести информацию')
+    print('0 - Завершить работу')
+
+    option = int(input('Введите номер пункта меню: '))
+    if option == 0:
+        break
